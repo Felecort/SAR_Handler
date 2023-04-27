c7951cd996296363493fe753f2e7e0c89f81b0a2
FriLDD <dotpointen@gmail.com>
2023-04-27 11:41:04 +0300

    added requirements and author

--- setup.py
+++ setup.py
@@ -5,6 +5,12 @@ setup(
     packages=find_packages(include=['sar_handler']),
     version='0.1.0',
     description='Handler SAR images',
-    author='Me',
+    author='Felecort',
+    # author_email="@gmail.com",
+    url="https://github.com/Felecort/SAR_Handler",
     license='MIT',
+    install_requires=["numpy"],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    test_suite='tests',
 )
