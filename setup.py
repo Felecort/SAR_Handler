3ae4eb90f9047b73a04c33b75c1e7868d6507802
FriLDD <dotpointen@gmail.com>
2023-04-27 11:27:49 +0300

    setup added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='sar_handler',
+    packages=find_packages(include=['sar_handler']),
+    version='0.1.0',
+    description='Handler SAR images',
+    author='Me',
+    license='MIT',
+)
